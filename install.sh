bc28a21d8d6354bc2992d7cc12f7add077bf94fc
Björn Friedrichs <bjoern@friedrichs1.de>
2017-01-30 18:04:35 +0100

    updated to make use of bash-me

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd bash
+git clone git@github.com:BFriedrichs/bash-me.git
+cd ..
+
 echo Symlinking dotfiles...
 rm -rf ~/.bash_profile
 ln -s "$PWD/bash/bash_profile" ~/.bash_profile
