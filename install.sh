daaf5e0fab6856455361015847e1a1b7ade85202
Björn Friedrichs <bjoern@friedrichs1.de>
2017-02-06 11:22:16 +0100

    added atom prefs

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-cd bash
-git clone git@github.com:BFriedrichs/bash-me.git
-cd ..
+if [  ! -d "bash/bash-me" ] ;then
+  cd bash
+  git clone git@github.com:BFriedrichs/bash-me.git
+  cd ..
+fi
 
 echo Symlinking dotfiles...
 rm -rf ~/.bash_profile
 ln -s "$PWD/bash/bash_profile" ~/.bash_profile
+
+rm -rf ~/.atom
+ln -s "$PWD/prefs/atom" ~/.atom
