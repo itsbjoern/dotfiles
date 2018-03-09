33c28bc4762d4624507482612a0edd8a97e55fa6
Björn Friedrichs <bjoern@friedrichs1.de>
2018-03-09 08:37:57 +0100

    update

---
+++ extras/docker.md
@@ -0,0 +1,4 @@
+## Mongo
+```
+docker run -p 27017:27017 -p 27018:27018 --name mongodb -v ~/.dockerdata/mongodb:/data/db -d mongo
+```
--- install.sh
+++ install.sh
@@ -6,8 +6,16 @@ if [  ! -d "bash/bash-me" ] ;then
   cd ..
 fi
 
+if [ -f ~/.git-completion.bash ]; then
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+fi
+
 echo Symlinking dotfiles...
 rm -rf ~/.bash_profile
-ln -s "$PWD/bash/bash_profile" ~/.bash_profile
+rm -rf ~/.gitignore-global
+rm -rf ~/.gitconfig
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+ln -s "$PWD/bash/bash_profile" ~/.bash_profile
+ln -s "$PWD/extras/.gitconfig" ~/.gitconfig
+ln -s "$PWD/extras/.gitignore-global" ~/.gitignore-global
+git config --global core.excludesfile '~/.gitignore-global'
