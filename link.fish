613e33f0ab99d1b46011832ab86e73f5155dd70f
Björn Friedrichs <mail@bjoernf.com>
2024-08-16 19:19:33 +0100

    Add grp change

--- link.fish
+++ link.fish
@@ -6,6 +6,8 @@ ln -sfvn $(pwd)/.gitconfig ~/.gitconfig
 ln -sfvn $(pwd)/.gitignore-global ~/.gitignore-global
 source ~/.config/fish/**/*.fish
 
+# sudo chgrp -R everyone "/Applications/Visual Studio Code.app"
+# sudo chmod -R g+w "/Applications/Visual Studio Code.app"
 
 # Set wezterm icons
 cp assets/wezterm.icns /Applications/WezTerm.app/Contents/Resources/terminal.icns
